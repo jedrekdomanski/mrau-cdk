e34292e8730831287f95e08633cae95303baee6b
Jędrzej Domański <jedrek.domanski@gmail.com>
2023-01-13 23:25:37 +0100

    Initial commit

---
+++ test/mrau-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MrauCdk from '../lib/mrau-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mrau-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MrauCdk.MrauCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
