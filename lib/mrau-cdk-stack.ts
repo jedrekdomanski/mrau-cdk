5c6ab1ab152e6ba2a1e01b90141a5d253579d45d
Jędrzej Domański <jedrek.domanski@gmail.com>
2023-01-13 23:37:13 +0100

    Configuration and initial imports (#1)

--- lib/mrau-cdk-stack.ts
+++ lib/mrau-cdk-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+import { CodeBuildProject } from './code_build'
 
 export class MrauCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MrauCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
